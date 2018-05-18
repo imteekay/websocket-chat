7e641d5b27c46a839f48a8c9a5df26aa1e114b92
TK <leandrotk100@gmail.com>
2018-05-17 23:48:40 -0300

    Implementing users chat

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,24 @@
+class MessagesController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    message = current_user.messages.build(message_params)
+
+    if message.save
+      ActionCable.server.broadcast 'messages',
+        message: message.content,
+        user: message.user.username
+      head :ok
+    else
+      redirect_to conversation_path(params[:conversation_id])
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message)
+          .permit(:content)
+          .merge(conversation_id: params[:conversation_id])
+  end
+end
