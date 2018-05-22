586f01d7ef5f17df6147cd9592b8d059e43a32e1
TK <leandrotk100@gmail.com>
2018-05-21 21:16:54 -0300

    Implement home with list of users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   mount ActionCable.server => '/cable'
 
+  root to: 'home#index'
+
   devise_for :users, controllers: {
     sessions: 'sessions',
     registrations: 'registrations'
